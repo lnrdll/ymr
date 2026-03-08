527cc1fc5e5c6618cc3068dc78da630ab905a67b
Robson Lunardelli <lunardelli@gfiber.com>
2026-03-07 19:35:39 -0600

    Clean up

--- internal/processor/types.go
+++
@@ -1,8 +0,0 @@
-package processor
-
-// RenderedOUtput holds the result of a single template render
-type RenderedOutput struct {
-	TargetFile   string
-	TemplateUsed string
-	Content      string
-}
